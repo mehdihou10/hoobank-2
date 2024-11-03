0ae76597132c83c25c79fe1c40e2c3dfe2bcdb72
mehdihou10 <108770033+mehdihou10@users.noreply.github.com>
2024-11-03 16:32:42 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const List = ()=>(
 
   <ul className="flex flex-col lg:flex-row items-center gap-[30px]
   ">
-        <li><a href="#" className='nav-item'>Acceuil</a></li>
+        <li><a href="#" className='nav-item'>HM</a></li>
         <li><a href="#features" className='nav-item'>Features</a></li>
         <li><a href="#product" className='nav-item'>Product</a></li>
         <li><a href="#clients" className='nav-item'>Clients</a></li>
